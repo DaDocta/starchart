c978a41f3bc3f1944725d4515296c333db27d11a
Beyond Understanding <127466825+b3yund@users.noreply.github.com>
2024-11-19 16:35:04 -0500

    Updated commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
 import Edit from './pages/Edit';
 import Everything from './pages/Everything';
-import BackgroundVideo from './components/BackgroundVideo';
 import { AuthProvider, AuthContext } from './AuthContext';
 import './styles/App.css';
-import './styles/Transitions.css';
 
 const App = () => {
   const location = useLocation();
@@ -16,34 +14,21 @@ const App = () => {
 
   return (
     <AuthProvider>
-      <div className="app-container">
-        <BackgroundVideo />
-        <SwitchTransition mode="out-in">
-          <CSSTransition
-            key={location.pathname}
-            classNames="fade"
-            timeout={{ enter: 1500, exit: 500 }}
-            unmountOnExit
-            appear
-            nodeRef={nodeRef}
-          >
-            <div className="page-content" ref={nodeRef}>
-              <AuthContext.Consumer>
-                {({ isAuthenticated }) => (
-                  <Routes location={location}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/portfolio/:name" element={<Portfolio />} />
-                    <Route path="/edit/:name" element={<Edit />} />
-                    <Route
-                      path="/everything"
-                      element={isAuthenticated ? <Everything /> : <Navigate to="/" replace />}
-                    />
-                  </Routes>
-                )}
-              </AuthContext.Consumer>
-            </div>
-          </CSSTransition>
-        </SwitchTransition>
+
+      <div className="page-content" ref={nodeRef}>
+        <AuthContext.Consumer>
+          {({ isAuthenticated }) => (
+            <Routes location={location}>
+              <Route path="/" element={<Home />} />
+              <Route path="/portfolio/:name" element={<Portfolio />} />
+              <Route path="/edit/:name" element={<Edit />} />
+              <Route
+                path="/everything"
+                element={isAuthenticated ? <Everything /> : <Navigate to="/" replace />}
+              />
+            </Routes>
+          )}
+        </AuthContext.Consumer>
       </div>
     </AuthProvider>
   );
