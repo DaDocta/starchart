994443d1a475b4fc143e292cd466c255bc6bb7a4
Beyond Understanding <127466825+b3yund@users.noreply.github.com>
2024-11-19 16:43:46 -0500

    Updated commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
@@ -8,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/starchart"> {/* Set basename to match GitHub Pages subdirectory */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
