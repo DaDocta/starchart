a13e33ba4bd94655a5b9cb42b6d55cfb6d7ab83d
DaDocta <garrett.strange@yahoo.com>
2024-12-30 14:25:00 -0500

    Updated commit

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,31 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import '../styles/BackgroundVideo.css';
 import Background from '../assets/Background.mp4';
 
 const BackgroundVideo = ({ fallbackText = 'Your browser does not support the video tag.' }) => {
-  const [videoLoaded, setVideoLoaded] = useState(false);
-
-  useEffect(() => {
-    const videoElement = document.querySelector('.background-video');
-    if (videoElement) {
-      videoElement.addEventListener('canplay', () => setVideoLoaded(true));
-      videoElement.addEventListener('error', () => setVideoLoaded(false));
-    }
-  }, []);
-
-  return videoLoaded ? (
+  return (
     <video className="background-video" autoPlay loop muted playsInline>
       <source src={Background} type="video/mp4" />
       {fallbackText}
     </video>
-  ) : (
-    <div className="fallback-message">Loading background...</div>
   );
 };
 
 BackgroundVideo.propTypes = {
-  fallbackText: PropTypes.string,
+  fallbackText: PropTypes.string, // Only fallbackText is configurable
 };
 
 export default BackgroundVideo;
